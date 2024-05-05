c9a672fb2432d25dcda509c735eeba95cf63b128
John Obi 20140237 <johnobi2003@gmail.com>
2024-05-05 11:48:27 +0300

    added v1 routes, env example und about.ejs

---
+++ Server/src/api/v1.ts
@@ -0,0 +1,76 @@
+import { Router } from 'express';
+const router = Router();
+import { saveText, SaveText } from '../controllers/database';
+import axios from 'axios';
+import { modelurl } from '../controllers/model';
+
+// localhost:8080/api/v1/
+
+// Define your headers
+const headers = {
+    'Content-Type': 'application/json',
+    'whoami': 'senml().'
+};
+
+// Define the request configuration
+const config = {
+headers: headers,
+};
+
+router.get('/', async (_req, res) => {
+    if(modelurl){
+        const data = await axios.get(modelurl);
+        if(data.status === 200){
+            res.json({"model":"live"});
+            return;
+        }
+    }else{
+        // 🚫 send the error message to the database??
+        res.status(403).json({"error": "model server not up"});
+        return;
+    }
+});
+
+router.post("/nmc", async (req, res) => {
+    const body = { "nmc": req.body.data};
+    try{
+        const datamodel = await axios.post(`${modelurl}/nmc`, body, config);
+        if (datamodel.status === 200 && datamodel.data) {
+            const modeldata: SaveText = datamodel.data;
+            modeldata.user = "anonymous";
+            modeldata.type = "nmc";
+            saveText(modeldata, req.body.data);
+            res.json(modeldata);
+        } else {
+            // 🚫 send the error message to the database??
+            console.error("Request failed or no data received.");
+        }
+    } catch (error: any) {
+        // 🚫 send the error message to the database??
+        console.error("An error occurred:", error);
+        res.json({"error": error.message})
+    }
+})
+
+router.post("/logreg", async (req, res) => {
+    const body = { "logreg": req.body.data};
+    try{
+        const datamodel = await axios.post(`${modelurl}/logreg`, body, config);
+        if (datamodel.status === 200 && datamodel.data) {
+            const modeldata: SaveText = datamodel.data;
+            modeldata.user = "anonymous";
+            modeldata.type = "logreg";
+            saveText(modeldata, req.body.data);
+            res.json(modeldata);
+        } else {
+            // 🚫 send the error message to the database??
+            console.error("Request failed or no data received.");
+        }
+    } catch (error: any) {
+        // 🚫 send the error message to the database??
+        console.error("An error occurred:", error);
+        res.json({"error": error.message})
+    }
+})
+
+export default router;
