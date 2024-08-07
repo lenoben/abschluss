63565c41e485c4cd68aeadabb892444a001a0065
lenoben <lenoben002@gmail.com>
2024-08-07 18:45:25 +0300

    first wanted mongodb, then reoved db altogether

--- Server/src/middleware/errorHandle.ts
+++ Server/src/middleware/errorHandle.ts
@@ -1,20 +1,22 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-import { prisma } from '../controllers/database';
+import { prisma } from "../controllers/database";
 
 const errorHandler = async (
-    err: Error,
-    _req: Request,
-    res: Response,
-    _next: NextFunction,
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
 ) => {
-    await prisma.error_log.create({ data: {
-        name: err.name,
-        message: err.message,
-        user_id: 1
-    }})
-    console.error(err.stack);
-    res.status(500).send(err.message);
+  await prisma.error_log.create({
+    data: {
+      name: err.name,
+      message: err.message,
+      user_id: "66b393083821775e2b731015",
+    },
+  });
+  console.error(err.stack);
+  res.status(500).send(err.message);
 };
 
 export default errorHandler;
